33b1e7acde865d2243e82dafcbc92ad4c5f5265a
asglasby <aisha.developer@gmail.com>
2015-04-16 09:12:03 -0400

    completed intial AJAX GET function

--- JavascriptHomeworkDay3/main.js
+++ JavascriptHomeworkDay3/main.js
@@ -1,1 +1,23 @@
-﻿
+﻿var firebaseUrl = "https://glowing-fire-7170.firebaseio.com/.json";
+
+var myAjaxGetToFirebase = function (callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', firebaseUrl, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log("success");
+            console.log(this.response);
+            callback(this.response);
+        } else {
+            console.error("this.response");
+            callback(this.response);
+        }
+    }
+    request.send();
+}
+
+var myAlert = function () {
+    console.log("I think my callback worked?");
+}
+
+myAjaxGetToFirebase(myAlert);
